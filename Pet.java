6db7743a5b6dbcef68280e507c91f7f5f723e662
chazhaze18 <CSMCHAZL@ljmu.ac.uk>
2024-02-08 18:19:47 +0000

    Create Pet.java

---
+++ Pet.java
@@ -0,0 +1,35 @@
+package vetmanagementsystem;
+
+import java.util.*;
+
+public abstract class Pet {
+    private String ref;
+    private String name;
+    private String speciesName;
+    private Date regDate;
+    private Surgery surgery;
+
+    public Pet(String ref, String name, String speciesName, Date regDate, Surgery surgery) {
+        this.ref = ref;
+        this.name = name;
+        this.speciesName = speciesName;
+        this.regDate = regDate;
+        this.surgery = surgery;
+    }
+
+    // Getters
+    public String getRef() {
+        return ref;
+    }
+
+    // Abstract methods
+    abstract void addBooking(Booking booking);
+    abstract void removeBooking(Booking booking);
+    abstract Booking nextBooking();
+    abstract List<Booking> allBookings();
+
+    // Relationships
+    public Surgery getSurgery() {
+        return surgery;
+    }
+}
